a5fe9a84793ea10629210d4481c01d3777e7ca64
sam-malanchuk <malanchuksam@gmail.com>
2019-08-17 13:14:21 -0400

    added endpoints to read single project by ID and all project list

--- projectsRouter.js
+++ projectsRouter.js
@@ -4,6 +4,27 @@ const Projects = require("./data/helpers/projectModel.js");
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    try {
+        const projects = await Projects.get();
+        res.status(200).json(projects);
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error retrieving the projects'
+        })
+    }
+});
 
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const project = await Projects.get(id);
+        res.status(200).json(project);
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error retrieving the projects'
+        })
+    }
+});
 
 module.exports = router;
