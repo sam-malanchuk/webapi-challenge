1df49c83e0c5fd56d78773838e5308788559441a
sam-malanchuk <malanchuksam@gmail.com>
2019-08-17 14:31:40 -0400

    created 2 endpoints in actions Router to get all actions and get action by ID. Created middleware to verify action ID exists

--- actionsRouter.js
+++ actionsRouter.js
@@ -4,4 +4,40 @@ const Actions = require("./data/helpers/actionModel.js");
 
 const router = express.Router();
 
+router.get('/:id', checkActionID, async (req, res) => {
+    res.status(200).json(req.action);
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const actions = await Actions.get();
+        res.status(200).json(actions);
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error retrieving the actions'
+        })
+    }
+});
+
+function checkActionID (req, res, next) {
+    const { id } = req.params;
+
+    Actions.get(id)
+        .then(action => {
+            if(action !== null) {
+                req.action = action;
+                next();
+            } else {
+                res.status(404).json({
+                    message: 'Action ID not found'
+                })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'Error retrieving the Action'
+            })
+        })
+}
+
 module.exports = router;
