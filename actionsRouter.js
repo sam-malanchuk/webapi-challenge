cb65008e9c19b5074a5089ae751dd52137567a97
sam-malanchuk <malanchuksam@gmail.com>
2019-08-17 14:36:32 -0400

    created api endpoint to allow creation of actions. If the action is missing required data, error is sent

--- actionsRouter.js
+++ actionsRouter.js
@@ -19,6 +19,24 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    const actionData = req.body;
+    try {
+        if(actionData.project_id && actionData.description && actionData.notes) {
+            const action = await Actions.insert(actionData);
+            res.status(201).json({action});
+        } else {
+            res.status(400).json({
+                message: 'Error data must include (project_id, description, notes) fields'
+            })
+        }
+    } catch (error) { 
+        res.status(500).json({
+            message: 'Error creating a new action'
+        })
+    }
+});
+
 function checkActionID (req, res, next) {
     const { id } = req.params;
 
