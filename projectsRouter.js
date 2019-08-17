751eb8815b3b9b48077ea38ce25b21d7b4a8064c
sam-malanchuk <malanchuksam@gmail.com>
2019-08-17 12:54:13 -0400

    set up projectsRouter and custom logger middleware

---
+++ projectsRouter.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const Projects = require("./data/helpers/projectModel.js");
+
+const router = express.Router();
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 
-const Projects = require("./data/helpers/projectModel.js");
+const projectsRouter = require("./projectsRouter.js");
 
 const server = express();
 
 server.use(express.json());
 
+server.use(logger);
+server.use('/api/projects', projectsRouter);
+
 server.get('/', (req, res) => {
     res.send("Server is up and running!");
-})
+});
+
+function logger(req, res, next) {
+    const timestamp = new Date().toLocaleString();
+    console.log(`A ${req.method} request at '${req.url}' at ${timestamp}`);
+    next();
+};
 
 module.exports = server;
