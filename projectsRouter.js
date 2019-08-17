9e95953c0ceddaa9d1896f29c7188992b729b815
sam-malanchuk <malanchuksam@gmail.com>
2019-08-17 13:20:55 -0400

    created custom middleware to check if ID exists

--- projectsRouter.js
+++ projectsRouter.js
@@ -15,10 +15,11 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', checkProjectID, async (req, res) => {
     const { id } = req.params;
     try {
         const project = await Projects.get(id);
+        console.log(project);
         res.status(200).json(project);
     } catch (error) {
         res.status(500).json({
@@ -26,5 +27,26 @@ router.get('/:id', async (req, res) => {
         })
     }
 });
+
+function checkProjectID (req, res, next) {
+    const { id } = req.params;
+
+    Projects.get(id)
+        .then(project => {
+            if(project !== null) {
+                req.project = project;
+                next();
+            } else {
+                res.status(404).json({
+                    message: 'Project ID not found'
+                })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'Error retrieving the projects'
+            })
+        })
+}
 
 module.exports = router;
